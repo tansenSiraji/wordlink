7c83e88e6f5fb01610b5db60289f2c46fcca298f
tansenSiraji <tansenananda@gmail.com>
2020-09-05 15:55:45 +0600

    littlebit progress

--- GameScreen.js
+++ GameScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TouchableOpacity } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TouchableOpacity,ImageBackground } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 // const Separator = () => (
@@ -30,67 +30,85 @@ class GameScreen extends Component {
     var box2 = this.state.box2
     var box3 = this.state.box3
     return (
-      <View>
-        <Text >
-          The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.
-      </Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        // onChangeText={text => onChangeText(text)}
-        value={this.state.box1}
-        />
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          value={this.state.box2}
-        />
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          value={this.state.box3}
-        />
-
-        {/* <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} > */}
-          <TouchableOpacity
-          onPress = {() => {
-            // Alert.alert("A")
-            box1 = 'B'
-            this.setState({box1: box1})
-          }}
-          >
-            <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }}>
-              <Text style = {{fontSize: 30, fontWeight: 'bold', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center'}}>B</Text>
+      //Main View
+      <View style={{
+        flex: 1,
+      }}>
+        <ImageBackground source={require('./Assets/Background.png')} style={styles.image}>
+        <View style={{
+          flex:3,
+          flexDirection: 'column',
+          // alignItems: "center",
+          justifyContent: 'space-evenly',
+          // backgroundColor: "blue"
+        }}>
+          {/*Upper Section View*/}
+          <View style={{
+            flexDirection: "row",
+            justifyContent: 'space-evenly',
+          }}>
+            {/* <Text>Upper Section</Text> */}
+            <View style={styles.middle}>
+              <Text style={{ fontSize: 30, fontWeight: 'bold', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center' }}>{this.state.box1}</Text>
             </View>
-            </TouchableOpacity>
+            <View style={styles.middle}>
+              <Text style={{ fontSize: 30, fontWeight: 'bold', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center' }}>{this.state.box2}</Text>
+            </View>
+            <View style={styles.middle}>
+              <Text style={{ fontSize: 30, fontWeight: 'bold', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center' }}>{this.state.box3}</Text>
+            </View>
+          </View>
+        </View>
 
+        <View style={{
+          flex: 2.2,
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          // backgroundColor: "red"
+        }}>
+          <View style={{
+            flexDirection: 'row',
+            alignItems: "center",
+            justifyContent: 'space-evenly',
+          }}>
+            {/* <Text>Lower Section</Text> */}
             <TouchableOpacity
-          onPress = {() => {
-            // Alert.alert("A")
-            box2 = 'A'
-            this.setState({box2: box2})
-          }}
-          >
-            <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }}>
-              <Text style = {{fontSize: 30, fontWeight: 'bold', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center'}}>A</Text>
-            </View>
+              onPress={() => {
+                // Alert.alert("A")
+                box1 = 'B'
+                this.setState({ box1: box1 })
+              }}
+            >
+              <View style={{ width: 50, height: 50, backgroundColor: 'beige' }}>
+                <Text style={{ fontSize: 30, fontWeight: 'bold', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center', borderWidth: 5 }}>B</Text>
+              </View>
             </TouchableOpacity>
-
             <TouchableOpacity
-          onPress = {() => {
-            // Alert.alert("A")
-            box3 = 'T'
-            this.setState({box3: box3})
-          }}
-          >
-            <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }}>
-              <Text style = {{fontSize: 30, fontWeight: 'bold', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center'}}>T</Text>
-            </View>
+              onPress={() => {
+                // Alert.alert("A")
+                box2 = 'A'
+                this.setState({ box2: box2 })
+              }}
+            >
+              <View style={{ width: 50, height: 50, backgroundColor: 'beige' }}>
+                <Text style={{ fontSize: 30, fontWeight: 'bold', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center', borderWidth: 5 }}>A</Text>
+              </View>
             </TouchableOpacity>
-          {/* </View> */}
-        
-
-
-
-
-      </View>
+            <TouchableOpacity
+              onPress={() => {
+                // Alert.alert("A")
+                box3 = 'T'
+                this.setState({ box3: box3 })
+              }}
+            >
+              <View style={{ width: 50, height: 50, backgroundColor: 'beige' }}>
+                <Text style={{ fontSize: 30, fontWeight: 'bold', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center', borderWidth: 5 }}>T</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        </ImageBackground>
+      </ View>
     )
   }
 }
@@ -116,6 +134,29 @@ const styles = StyleSheet.create({
     borderBottomColor: '#737373',
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
+  middle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "beige",
+    borderWidth: 5,
+    // shadowColor: "#FFFFFF",
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 3,
+    // },
+    // shadowOpacity: 0.58,
+    // shadowRadius: 2,
+    
+    // elevation: 10,
+    
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+
 });
 
 export default GameScreen;
--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 let fs = require("fs")
 
 var words = []
-let text = fs.readFileSync("word.js").toString().split(" ")
+let text = fs.readFileSync("tenThouWords.txt").toString().split(" ")
 for(var i = 0; i < text.length; i++){
     words.push(text[i])
 }
 
 try {
-    fs.writeFileSync('WordLists.js', JSON.stringify(words));
+    fs.writeFileSync('WordLists3.js', JSON.stringify(words));
   } catch(err) {
     
     console.error(err);
